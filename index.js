ad59324e87593efdf775ec23722a9ac202733c24
beefybroccoli <tedkimemail@yahoo.com>
2021-10-28 16:04:34 -0700

    ...progress

--- index.js
+++ index.js
@@ -38,3 +38,12 @@ There are two ways to provide input into a node application:
 _command line argument (node index.js 45 56 67)
 _using environmental variable to pass the variables into a node application
 */
+
+/*
+npx gitignore node - create a git ignore for node package
+git init -create a git repo
+npm i express
+npm i
+npx eslint --init, then select default
+
+*/
